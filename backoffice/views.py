c5e66c89bc25e214d8c862ce555f27b53dc7d391
Deries <user@gmail.com>
2018-11-23 22:03:16 -0500

    Modificaciones

--- backoffice/views.py
+++ backoffice/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    return render(request, 'index.html')
+
+def listado(request):
+    return render(request, "crud/listar.html")
+
+def agregar(request):
+    return render(request, "crud/agregar.html")
--- clase05_bootstrap/urls.py
+++ clase05_bootstrap/urls.py
@@ -16,6 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from backoffice.views import *
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', index, name='index'),
+    path('listado/', listado, name='listado'),
+    path('agregar/', agregar, name='agregar'),
 ]
